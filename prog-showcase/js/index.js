439114cdbda3a1399e118fec5f6d899765aea813
Brandon Mikel <BrandonMikel@gmail.com>
2019-05-31 10:54:50 +1200

    fixing data-rides

--- prog-showcase/js/index.js
+++ prog-showcase/js/index.js
@@ -1,13 +1,3 @@
-$('.carousel-banners-desktop').carousel();
-$('.carousel-featuresets-desktop').carousel();
-$('.carousel-stories-desktop').carousel();
-$('.carousel-footers-desktop').carousel();
-
-$('.carousel-banners-mobile').carousel();
-$('.carousel-featuresets-mobile').carousel();
-$('.carousel-stories-mobile').carousel();
-$('.carousel-footers-mobile').carousel();
-
 $(".carousel-control-prev").click(function(){
     $(".carousel-banners-mobile").carousel("prev");
 });
