df72f0a7a06cb2390e77be6e61e5e7ae37b80d41
Brandon Mikel <BrandonMikel@gmail.com>
2019-05-30 17:05:01 +1200

    separating carousel controls

--- prog-showcase/js/index.js
+++ prog-showcase/js/index.js
@@ -1,4 +1,13 @@
-$('.carousel-banners').carousel()
-$('.carousel-featuresets').carousel()
-$('.carousel-stories').carousel()
-$('.carousel-footers').carousel()
+$('.carousel-banners-desktop').carousel();
+$('.carousel-featuresets-desktop').carousel();
+$('.carousel-stories-desktop').carousel();
+$('.carousel-footers-desktop').carousel();
+
+$('.carousel-banners-mobile').carousel();
+$('.carousel-featuresets-mobile').carousel();
+$('.carousel-stories-mobile').carousel();
+$('.carousel-footers-mobile').carousel();
+
+$(".carousel-control-prev").click(function(){
+    $(".carousel-banners-mobile").carousel("prev");
+}
