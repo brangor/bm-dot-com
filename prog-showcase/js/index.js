65d475bdf4083a470aa5195d98dc0ca60b197a1a
Brandon Mikel <BrandonMikel@gmail.com>
2019-06-04 17:32:42 +1200

    updating

--- prog-showcase/js/index.js
+++ prog-showcase/js/index.js
@@ -1,22 +1,27 @@
 $('.btn-group').on('click', '.btn', function() {
   $(this).addClass('active').siblings().removeClass('active');
 });
+
 $('#optionBanner').click(function() {
   $('.section').addClass('zonked');
   $('.banner-section').removeClass('zonked');
 });
+
 $('#optionFeatureSet').click(function() {
   $('.section').addClass('zonked');
   $('.featureset-section').removeClass('zonked');
 });
+
 $('#optionSummary').click(function() {
   $('.section').addClass('zonked');
   $('.summary-section').removeClass('zonked');
 });
+
 $('#optionStories').click(function() {
   $('.section').addClass('zonked');
   $('.stories-section').removeClass('zonked');
 });
+
 $('#optionFooter').click(function() {
   $('.section').addClass('zonked');
   $('.footer-section').removeClass('zonked');
