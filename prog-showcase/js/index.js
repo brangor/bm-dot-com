c400c9946086dec9e96e1c6cb94919dbac52f0b2
Brandon Mikel <BrandonMikel@gmail.com>
2019-06-04 17:05:31 +1200

    updating most of the page from codepen work

--- prog-showcase/js/index.js
+++ prog-showcase/js/index.js
@@ -1,5 +1,23 @@
-$(".carousel-control-prev").click(function(){
-    alert('You clicked previous!');
-    /*$('".carousel-banners-desktop").carousel("prev");'*/
+$('.btn-group').on('click', '.btn', function() {
+  $(this).addClass('active').siblings().removeClass('active');
 });
-
+$('#optionBanner').click(function() {
+  $('.section').addClass('zonked');
+  $('.banner-section').removeClass('zonked');
+});
+$('#optionFeatureSet').click(function() {
+  $('.section').addClass('zonked');
+  $('.featureset-section').removeClass('zonked');
+});
+$('#optionSummary').click(function() {
+  $('.section').addClass('zonked');
+  $('.summary-section').removeClass('zonked');
+});
+$('#optionStories').click(function() {
+  $('.section').addClass('zonked');
+  $('.stories-section').removeClass('zonked');
+});
+$('#optionFooter').click(function() {
+  $('.section').addClass('zonked');
+  $('.footer-section').removeClass('zonked');
+});
